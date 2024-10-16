d5c9f9055f186fe18977e8230cc0d61e01c265f5
lbrbence169 <leber.bence08@gmail.com>
2024-10-16 17:39:03 +0200

    Otthon

--- Bence_Bejelentkezve.py
+++ Bence_Bejelentkezve.py
@@ -5,7 +5,7 @@ def alkalmazás():
     siker_ablak.geometry("700x500")
     siker_ablak.configure(bg="black")
     siker_ablak.title("Siker")
-    siker_cím = Label(siker_ablak, text="Bence profilja🕸", bg="black", fg="#00FF9C", font=('Times', 50, 'bold'))
+    siker_cím = Label(siker_ablak, text="Bence profilja🕸", fg="black", bg="#00FF9C", font=('Times', 50, 'bold'))
     siker_cím.grid(row=0, columnspan=1, pady=10, padx=120)
 
     # abelepes_sikeres = Label(siker_ablak, text="A belépés sikeres volt", bg="black", fg="red", font=('Helvetica', 10, 'bold'))
@@ -14,14 +14,28 @@ def alkalmazás():
     # ertettem_gomb.grid(row=2, columnspan=2, pady=3, padx=5)
     
     #írás
-    chatfal = Label(siker_ablak, text="Írjon ide valamit:", bg="black", fg="red", font=('Helvetica', 15, 'bold'))
-    chatfal.grid(row=3, pady=10, padx=50)
+    chatfal = Label(siker_ablak, text="Írja ide az üzenetét", bg="black", fg="red", font=('Helvetica', 15, 'bold'))
+    chatfal.grid(row=3, pady=10, padx=(0,550))
     ide_irjon = Entry(siker_ablak, width=20, bg="#00FF9C")
-    ide_irjon.grid(row=3, columnspan=1, pady=10, padx=2)
+    ide_irjon.grid(row=4, pady=0, padx=(0,560))
+    
+    
+    def megnyomva():
+        try:
+            with open("üzenetek.txt", 'w') as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
+                    üzenetek = ide_irjon.get()
+                    print(üzenetek, sep='\n', file=fajl)
+        except:
+            print('Nem találjuk az adatokat')
+        # kiiras = Label(siker_ablak, text=üzenetek, fg="red", bg="black", font=('Times', 10, 'bold'))
+        # kiiras.grid(row=4, pady=0, padx=(0,300))
+    bekuldesgomb = Button(siker_ablak, text ="=>", bg="#00FF9C", font="Times 8",  borderwidth=3, command=megnyomva).grid(row=4, pady=0, padx=(0,400))
+
     #kilépés
-    vissza_fooldalra = Button(siker_ablak, text="Vissza a főoldalra", bg="red", font="sans 23 bold", command=siker_ablak.destroy)
-    vissza_fooldalra.grid(row=4, columnspan=1,pady=15,padx=20)
+    vissza_fooldalra = Button(siker_ablak, text="Vissza a főoldalra", bg="red", font="sans 10 bold", command=siker_ablak.destroy)
+    vissza_fooldalra.grid(row=6, columnspan=1,pady=15,padx=20)
     
 
 
     siker_ablak.mainloop()
+alkalmazás()
