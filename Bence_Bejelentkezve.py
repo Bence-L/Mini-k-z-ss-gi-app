af4b6680e35385109d695c033f9b51ba0190b387
lbrbence169 <leber.bence08@gmail.com>
2024-10-16 18:20:40 +0200

    LISTA

--- Bence_Bejelentkezve.py
+++ Bence_Bejelentkezve.py
@@ -18,25 +18,41 @@ def alkalmazás():
     chatfal.grid(row=3, pady=10, padx=(0,550))
     ide_irjon = Entry(siker_ablak, width=20, bg="#00FF9C")
     ide_irjon.grid(row=4, pady=0, padx=(0,560))
-    
-    
+
     def megnyomva():
         üzenetek = ide_irjon.get()
         elküldve = Label(siker_ablak, text="Elküldve", bg="black", fg="red", font=('Times', 10, 'bold'))
         elküldve.grid(row=5, pady=10, padx=(0,550))
+        def eltavolit():
+            elküldve.destroy()
+        siker_ablak.after(2000, eltavolit)
         try:
-            with open("üzenetek.txt", 'a') as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
+            with open("üzenetek.txt", 'a', encoding="utf-8") as fajl:
                     üzenetek = ide_irjon.get()
-                    print(üzenetek+" 2", sep='\n', file=fajl)
+                    print(üzenetek+"; BENCE", sep='\n', file=fajl)
         except:
             print('Nem találjuk az adatokat')
-        # kiiras = Label(siker_ablak, text=üzenetek, fg="red", bg="black", font=('Times', 10, 'bold'))
-        # kiiras.grid(row=4, pady=0, padx=(0,300))
+        
     bekuldesgomb = Button(siker_ablak, text ="=>", bg="#00FF9C", font="Times 8",  borderwidth=3, command=megnyomva).grid(row=4, pady=0, padx=(0,400))
-
+    
+    def beszelgetni():
+        kiüzent = []
+        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
+                    for sor in fajl:
+                        adat = sor.strip().split(';')
+                        kiüzent.append({
+                        'Üzenet' : adat[0],
+                        'Kiküldte' : adat[1]
+                })
+        print(kiüzent)
+                
+    beszelgetni()
+            
+                          
+    
     #kilépés
     vissza_fooldalra = Button(siker_ablak, text="Vissza a főoldalra", bg="red", font="sans 10 bold", command=siker_ablak.destroy)
-    vissza_fooldalra.grid(row=6, columnspan=1,pady=15,padx=20)
+    vissza_fooldalra.grid(row=8, columnspan=1,pady=15,padx=20)
     
 
 
