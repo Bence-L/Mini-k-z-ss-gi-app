7aaedc389a7720f74ac6504eab360653a73f7ac7
Bence-L <11c-leber@ipari.vein.hu>
2024-10-18 13:50:25 +0200

    Reggel

--- Bence_Bejelentkezve.py
+++ Bence_Bejelentkezve.py
@@ -18,9 +18,9 @@ def alkalmazás():
     chatfal.grid(row=3, pady=10, padx=(200,200))
     ide_irjon = Entry(siker_ablak, width=20, bg="#00FF9C")
     ide_irjon.grid(row=4, pady=0, padx=(200,200))
-    rightcisechat = Frame(siker_ablak, width=682, height=500, bg="#656966", borderwidth=60)
-    rightcisechat.grid(row=2, pady=10,padx=(10,10))
-
+    # rightcisechat = Frame(siker_ablak, width=682, height=500, bg="#656966", borderwidth=60)
+    # rightcisechat.grid(row=9, pady=10,padx=(10,10))
+    
     def megnyomva():
         üzenetek = ide_irjon.get()
         elküldve = Label(siker_ablak, text="Elküldve", bg="black", fg="red", font=('Times', 10, 'bold'))
@@ -34,27 +34,42 @@ def alkalmazás():
                     print(üzenetek+"; BENCE", sep='\n', file=fajl)
         except:
             print('Nem találjuk az adatokat')
+        beszelgetni()
+        iras()
+        
+    def beszelgetni():
+        global kiüzent
+        kiüzent = []
+        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
+                for sor in fajl:
+                    adat = sor.strip().split(';')
+                    kiüzent.append({
+                    'Üzenet' : adat[0],
+                    'Kiküldte' : adat[1]
+            })
+        print(kiüzent)
+        
+        
+        
+    def iras():   
+        sorkoz = 1
+        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl:
+            for i in kiüzent:
+                if i['Kiküldte']=="BENCE":
+                    uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20",fg="red",bg="#656966")
+                    uzenetlabel.grid(row=9,pady=sorkoz, padx="300,200")
+                    sorkoz+=1
+                elif i['Kiküldte']=="ÁRON":
+                    uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20",fg="red",bg="#656966")
+                    uzenetlabel.grid(row=9,pady=sorkoz, padx="300,200")
+                    sorkoz+=1
+        print('asdas')
         
     bekuldesgomb = Button(siker_ablak, text ="=>", bg="#00FF9C", font="Times 8",  borderwidth=3, command=megnyomva).grid(row=4, pady=0, padx=(280,130))
     
-    def kuldes():
-        sor = 0.5
-        amitkap = ide_irjon.get()
-        if megnyomva():
-            uzenet = Label(siker_ablak, text=amitkap,bg="black",fg="red", font="Times 8")
-            uzenet.grid(row=2, pady=sor,padx=(200,200))
-            sor +=sor
-                
-    kuldes()
-                
-    # beszelgetni()
-    
-            
-                          
-    
     #kilépés
     vissza_fooldalra = Button(siker_ablak, text="Vissza a főoldalra", bg="red", font="sans 10 bold", command=siker_ablak.destroy)
-    vissza_fooldalra.grid(row=8, pady=15, padx=(200,200))
+    vissza_fooldalra.grid(row=10, pady=15, padx=(200,200))
     
 
 
