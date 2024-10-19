f486d71c4cfc589079a1c6fb129befe319fe1bf5
lbrbence169 <leber.bence08@gmail.com>
2024-10-19 07:29:37 +0200

    KÜLDÉS MEGY

--- Aron_Bejelentkezve.py
+++ Aron_Bejelentkezve.py
@@ -1,56 +1,76 @@
-from tkinter import *
+from tkinter import Tk, Label, Entry, Button
 
 def ablakja():
     ARON_ablak = Tk()
-    ARON_ablak.geometry("700x500")
+    ARON_ablak.geometry("700x900")
     ARON_ablak.configure(bg="black")
     ARON_ablak.title("ÁRON VAGYOK")
     ARON_cím = Label(ARON_ablak, text="ÁRON profilja🎁", fg="black", bg="#00FF9C", font=('Times', 50, 'bold'))
     ARON_cím.grid(row=0, columnspan=1, pady=10, padx=120)
 
-    # abelepes_sikeres = Label(siker_ablak, text="A belépés sikeres volt", bg="black", fg="red", font=('Helvetica', 10, 'bold'))
-    # abelepes_sikeres.grid(row=2, columnspan=1,pady=20, padx=(220,360))
-    # ertettem_gomb = Button(siker_ablak, text="Értettem!", fg="red",bg="black", font="sans 10 bold", command=abelepes_sikeres.destroy)
-    # ertettem_gomb.grid(row=2, columnspan=2, pady=3, padx=5)
-    
-    #írás
     ARON_chatfal = Label(ARON_ablak, text="Írja ide az üzenetét", bg="black", fg="red", font=('Helvetica', 15, 'bold'))
     ARON_chatfal.grid(row=3, pady=10, padx=(0,550))
     ARON_ide_irjon = Entry(ARON_ablak, width=20, bg="#00FF9C")
     ARON_ide_irjon.grid(row=4, pady=0, padx=(0,560))
 
     def megnyomva():
-        ARON_üzenetek = ARON_ide_irjon.get()
-        ARON_elküldve = Label(ARON_ablak, text="Elküldve", bg="black", fg="red", font=('Times', 10, 'bold'))
-        ARON_elküldve.grid(row=5, pady=10, padx=(0,550))
+        üzenet = ARON_ide_irjon.get()
+        elküldve = Label(ARON_ablak, text="Elküldve", bg="black", fg="red", font=('Times', 10, 'bold'))
+        elküldve.grid(row=5, pady=10, padx=(0,550))
+        
         def eltavolit():
-            ARON_elküldve.destroy()
+            elküldve.destroy()
+        
         ARON_ablak.after(2000, eltavolit)
+        
         try:
             with open("üzenetek.txt", 'a', encoding="utf-8") as fajl:
-                    üzenetek = ARON_ide_irjon.get()
-                    print(üzenetek+"; ÁRON", sep='\n', file=fajl)
-        except:
-            print('Nem találjuk az adatokat')
+                fajl.write(üzenet + "; ÁRON\n")
+        except Exception as e:
+            print('Nem találjuk az adatokat', e)
         
-    ARON_bekuldesgomb = Button(ARON_ablak, text ="=>", bg="#00FF9C", font="Times 8",  borderwidth=3, command=megnyomva).grid(row=4, pady=0, padx=(0,400))
+        ARON_ide_irjon.delete(0, 'end')
+        beszelgetni()
+        iras()
     
     def beszelgetni():
+        global kiüzent
         kiüzent = []
-        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
-                    for sor in fajl:
-                        adat = sor.strip().split(';')
+        
+        try:
+            with open("üzenetek.txt", 'r', encoding="utf-8") as fajl:
+                for sor in fajl:
+                    adat = sor.strip().split(';')
+                    if len(adat) == 2:
                         kiüzent.append({
-                        'Üzenet' : adat[0],
-                        'Kiküldte' : adat[1]
-                })
-        print(kiüzent)
-                
+                            'Üzenet': adat[0],
+                            'Kiküldte': adat[1]
+                        })
+        except FileNotFoundError:
+            print("Az üzenetek.txt fájl nem található.")
+
+    def iras():
+        sorkoz = 6  
+        for widget in ARON_ablak.grid_slaves():
+            if int(widget.grid_info()["row"]) >= 6:
+                widget.grid_forget()
+        
+        for i in kiüzent:
+            if i['Kiküldte'] == "ÁRON":
+                uzenetlabel = Label(ARON_ablak, text=i['Üzenet'], font="Times 20", fg="white", bg="#00FF9C")
+                uzenetlabel.grid(row=sorkoz, column=0, pady=5, padx=(10, 0))
+            else:
+                uzenetlabel = Label(ARON_ablak, text=i['Üzenet'], font="Times 20", fg="red", bg="#656966")
+                uzenetlabel.grid(row=sorkoz, column=1, pady=5, padx=(450, 0))
+            sorkoz += 1
+
+    bekuldesgomb = Button(ARON_ablak, text="=>", bg="#00FF9C", font="Times 8", borderwidth=3, command=megnyomva)
+    bekuldesgomb.grid(row=4, pady=0, padx=(0,440))
+
     beszelgetni()
-            
-                          
+    iras()
     
-    #kilépés
-    vissza_fooldalra = Button(ARON_ablak, text="Vissza a főoldalra", bg="red", font="sans 10 bold", command=ARON_ablak.destroy)
-    vissza_fooldalra.grid(row=8, columnspan=1,pady=15,padx=20)
+    ARON_ablak.mainloop()
+
+ablakja()
     
--- Bence_Bejelentkezve.py
+++ Bence_Bejelentkezve.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Tk, Label, Entry, Button
 
 def alkalmazás():
     siker_ablak = Tk()
@@ -8,69 +8,68 @@ def alkalmazás():
     siker_cím = Label(siker_ablak, text="Bence profilja🕸", fg="red", bg="black", font=('Times', 50, 'bold'))
     siker_cím.grid(row=0, columnspan=1, pady=10, padx=60)
 
-    # abelepes_sikeres = Label(siker_ablak, text="A belépés sikeres volt", bg="black", fg="red", font=('Helvetica', 10, 'bold'))
-    # abelepes_sikeres.grid(row=2, columnspan=1,pady=20, padx=(220,360))
-    # ertettem_gomb = Button(siker_ablak, text="Értettem!", fg="red",bg="black", font="sans 10 bold", command=abelepes_sikeres.destroy)
-    # ertettem_gomb.grid(row=2, columnspan=2, pady=3, padx=5)
-    
-    #írás
     chatfal = Label(siker_ablak, text="Írja ide az üzenetét:", bg="black", fg="red", font=('Helvetica', 15, 'bold'))
-    chatfal.grid(row=3, pady=10, padx=(200,200))
+    chatfal.grid(row=3, pady=10, padx=(0,550))
     ide_irjon = Entry(siker_ablak, width=20, bg="#00FF9C")
-    ide_irjon.grid(row=4, pady=0, padx=(200,200))
-    # rightcisechat = Frame(siker_ablak, width=682, height=500, bg="#656966", borderwidth=60)
-    # rightcisechat.grid(row=9, pady=10,padx=(10,10))
-    
+    ide_irjon.grid(row=4, pady=0, padx=(0,560))
+
     def megnyomva():
-        üzenetek = ide_irjon.get()
+        üzenet = ide_irjon.get()
         elküldve = Label(siker_ablak, text="Elküldve", bg="black", fg="red", font=('Times', 10, 'bold'))
-        elküldve.grid(row=5, pady=10, padx=(200,200))
+        elküldve.grid(row=5, pady=10, padx=(0,550))
+        
         def eltavolit():
             elküldve.destroy()
+        
         siker_ablak.after(2000, eltavolit)
+        
         try:
             with open("üzenetek.txt", 'a', encoding="utf-8") as fajl:
-                    üzenetek = ide_irjon.get()
-                    print(üzenetek+"; BENCE", sep='\n', file=fajl)
-        except:
-            print('Nem találjuk az adatokat')
+                fajl.write(üzenet + "; BENCE\n")
+        except Exception as e:
+            print('Nem találjuk az adatokat', e)
+        
+        ide_irjon.delete(0, 'end')
         beszelgetni()
         iras()
-        
+    
     def beszelgetni():
         global kiüzent
         kiüzent = []
-        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl: #encodinggal fur betűk lettek!!!!!!!!!!!
+        
+        try:
+            with open("üzenetek.txt", 'r', encoding="utf-8") as fajl:
                 for sor in fajl:
                     adat = sor.strip().split(';')
-                    kiüzent.append({
-                    'Üzenet' : adat[0],
-                    'Kiküldte' : adat[1]
-            })
-        print(kiüzent)
-        
-        
-        
-    def iras():   
-        sorkoz = 1
-        with open("üzenetek.txt", 'r', encoding="utf-8") as fajl:
-            for i in kiüzent:
-                if i['Kiküldte']=="BENCE":
-                    uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20",fg="red",bg="#656966")
-                    uzenetlabel.grid(row=9,pady=sorkoz, padx="300,200")
-                    sorkoz+=1
-                elif i['Kiküldte']=="ÁRON":
-                    uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20",fg="red",bg="#656966")
-                    uzenetlabel.grid(row=9,pady=sorkoz, padx="300,200")
-                    sorkoz+=1
-        print('asdas')
-        
-    bekuldesgomb = Button(siker_ablak, text ="=>", bg="#00FF9C", font="Times 8",  borderwidth=3, command=megnyomva).grid(row=4, pady=0, padx=(280,130))
-    
-    #kilépés
-    vissza_fooldalra = Button(siker_ablak, text="Vissza a főoldalra", bg="red", font="sans 10 bold", command=siker_ablak.destroy)
-    vissza_fooldalra.grid(row=10, pady=15, padx=(200,200))
-    
+                    if len(adat) == 2:
+                        kiüzent.append({
+                            'Üzenet': adat[0],
+                            'Kiküldte': adat[1]
+                        })
+        except FileNotFoundError:
+            print("Az üzenetek.txt fájl nem található.")
 
+    def iras():
+        sorkoz = 6  
+        for widget in siker_ablak.grid_slaves():
+            if int(widget.grid_info()["row"]) >= 6:
+                widget.grid_forget()
+        
+        for i in kiüzent:
+            if i['Kiküldte'] == "BENCE":
+                uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20", fg="black", bg="#00FF9C")
+                uzenetlabel.grid(row=sorkoz, column=1, pady=5, padx=(450, 0))
+            else:
+                uzenetlabel = Label(siker_ablak, text=i['Üzenet'], font="Times 20", fg="red", bg="#656966")
+                uzenetlabel.grid(row=sorkoz, column=0, pady=5, padx=(10, 0))
+            sorkoz += 1
 
+    bekuldesgomb = Button(siker_ablak, text="=>", bg="#00FF9C", font="Times 8", borderwidth=3, command=megnyomva)
+    bekuldesgomb.grid(row=4, pady=0, padx=(0,440))
+
+    beszelgetni()
+    iras()
+    
     siker_ablak.mainloop()
+
+alkalmazás()
