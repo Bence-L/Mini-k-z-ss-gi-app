12a2fe55e949174143dd364b06a23f461817e29e
lbrbence169 <leber.bence08@gmail.com>
2024-11-04 19:20:54 +0100

    Képek alakítása

--- BENCE_GALÉRIA.py
+++ BENCE_GALÉRIA.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from tkinter import ttk
 
 # A galéria ablak megnyitása
 def bencegaleria():
@@ -8,56 +8,60 @@ def bencegaleria():
     GalériaB.title("Bence képei")
     GalériaB.configure(bg="black")
 
-    #A képek elérési útvonala
+    # A képek elérési útvonala
     kepek = [
-        r"elsokep.jpg",
-        r"masodikkep.jpg",
-        r"harmadikkep.jpg",
-        r"hetedik.webp",
-        r"nyolcadik.jpg"
+        r"elsokep.png",
+        r"masodikkep.png",
+        r"harmadikkep.png",
+        r"hetedik.png",
+        r"nyolcadik.png"
     ]
     
-    global index, img 
+    global index, img_list, img_label, textOfStatusbar
     index = 0
 
-    #Képek betöltése és megjelenítése
-    def load_image():
-        global index, img 
-        img = Image.open(kepek[index])
-        img = img.resize((500, 500), Image.LANCZOS)
-        img = ImageTk.PhotoImage(img)
-        img_label.config(image=img)
-        img_label.image = img
-    
-    #Lépés a következő képre
-    def next_image():
-        global index
-        index = (index + 1) % len(kepek)
-        load_image()
-    #Lépés az előző képre
-    def prev_image():
-        global index
-        index = (index - 1) % len(kepek)
-        load_image()
+    # Képek betöltése
+    img_list = [PhotoImage(file=kep) for kep in kepek]
 
+    
+    
+
+    
     # Kép megjelenítésére szolgáló címke létrehozása
     img_label = Label(GalériaB, bg="black")
     img_label.grid(row=1, column=0, columnspan=3, padx=50, pady=10)
+    
+    # Kép betöltése és megjelenítése
+    def load_image():
+        img_label.config(image=img_list[index])
 
-    #Első kép betöltése
-    load_image()  
+    # Lépés a következő képre
+    def next_image():
+        global index
+        index = (index + 1) % len(img_list)
+        load_image()
+        
+
+    # Lépés az előző képre
+    def prev_image():
+        global index
+        index = (index - 1) % len(img_list)
+        load_image()
+        
+
+    # Első kép betöltése
+    load_image()
+
     emoji = Label(GalériaB, text="Bence képei", bg="black", fg="#28e8fa", font=('Comic Sans', 30, 'bold'))
     emoji.grid(row=0, column=1, pady=5, padx=90)
 
     # Navigációs gombok létrehozása és elhelyezése a grid segítségével
-    #Lépés az előző képre
     prev_button = Button(GalériaB, text="<==", bg="#28e8fa", font="Times 20 bold", borderwidth=10, command=prev_image)
     prev_button.grid(row=2, column=0, padx=20, pady=10)
     
-    #Lépés a következő képre
     next_button = Button(GalériaB, text="==>", bg="#28e8fa", font="Times 20 bold", borderwidth=10, command=next_image)
     next_button.grid(row=2, column=2, padx=20, pady=10)
-    
-    #bezáró gomb
-    bezaro_gomb = Button(GalériaB, text="Bezárás", bg="#ed735a", font="sans 23 bold", command=GalériaB.destroy).grid(row=3, column=1,pady=5,padx=20)
 
+    # Bezáró gomb
+    bezaro_gomb = Button(GalériaB, text="Bezárás", bg="#ed735a", font="sans 23 bold", command=GalériaB.destroy)
+    bezaro_gomb.grid(row=3, column=1, pady=5, padx=20)
